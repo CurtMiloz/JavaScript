9b331a13f0d956ac8d0cc1273d2c6807e59c30ab
Greg Barkans <gregbarkans@gmail.com>
2015-03-11 00:30:31 -0400

    Update resume.js

--- resume.js
+++ resume.js
@@ -5,7 +5,7 @@
 function resumeGame() {
     if (localStorage.getItem('nextMove')) {
       redOrBlue = JSON.parse(localStorage.getItem('nextMove'));
-      if (redOrBlue == red) {
+      if (redOrBlue == red) { // variable holding who's turn it is
         document.images["red"].src = "img/red_a.png" // project specific
       } else {
         document.images["blue"].src = "img/blue_a.png" // project specific 
@@ -16,9 +16,9 @@ function resumeGame() {
 
         for (var i = 0; i < 6; i++) {
             for (var j = 0; j < 7; j++) {
-                connectFour.board[i][j] = temp[i][j];
+                connectFour.board[i][j] = temp[i][j]; // update board array
                 if (temp[i][j] != "") {
-                    updateBoard(i, j, temp[i][j]);
+                    updateBoard(i, j, temp[i][j]);   // draw to screen
                 }
             }
         }
